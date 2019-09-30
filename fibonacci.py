7cb8f68caecd7b058e1c16107355e58975780728
kookmin1916 <55117761+kookmin1916@users.noreply.github.com>
2019-09-30 10:56:18 +0900

    fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -5,14 +5,14 @@ O = numpy.matrix([[1, 1], [1, 0]])
 I = numpy.matrix([[1, 0], [0, 1]])
 
 def iterfibo(n):
-    ret = I;
-    for i in range(n - 1):
+    ret = O * O
+    for i in range(n - 2):
         ret *= O
     return ret[0, 0]
 
-def fibo(n, M = I):
+def fibo(n, M = O):
     if n == 1:
-        return I
+        return M
     return fibo(n - 1, M * O)
 
 while True:
@@ -20,10 +20,10 @@ while True:
     if nbr == -1:
         break
     ts = time.time()
-    fibonumber = iterfibo(nbr)
+    fibonumber = iterfibo(nbr - 1)
     ts = time.time() - ts
     print("IterFibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
     ts = time.time()
-    fibonumber = fibo(nbr)[0, 0]
+    fibonumber = fibo(nbr - 1)[0, 0]
     ts = time.time() - ts
     print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
